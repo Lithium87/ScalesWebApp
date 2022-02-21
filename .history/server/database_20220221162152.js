b697ce859b01d8aabd619daa306f03db689cb012
Lithium@87 <cibs@abv.bg>
2022-02-21 16:25:05 +0200

    Create database

--- .history/server/database_20220221162152.js
+++ .history/server/database_20220221162152.js
@@ -2,7 +2,7 @@ const {Sequelize} = require ('sequelize');
 
 const sequelize = new Sequelize ('web-scales', null, null, {
   dialect: 'sqlite',
-  storage: './server/WebScales.sqlite',
+  storage: './WebScales.sqlite',
 });
 
 module.exports = sequelize;
